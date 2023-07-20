55940efc10dc13cdc6afdc69b02cc45f81fe7ff4
ksu <ksush3@mail.ru>
2023-07-20 15:08:37 +0300

    внесли изменения в задачу19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -7,16 +7,23 @@ int n = Convert.ToInt32(Console.ReadLine());
 if (n < 10000 || n > 99999)
 {
     Console.WriteLine("Это не пятизначное число! Введите пятизначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+   
 }
 
-int n1 = n / 10000;
-int n2 = n / 1000 % 10;
-int n3 = n / 10 % 10;
-int n4 = n % 10;
 
-if (n1 == n4 && n2 == n3)
+if (n > 9999 && n < 100000)
 {
-    Console.WriteLine("Это палиндром!");
+    int n1 = n / 10000;
+    int n2 = n / 1000 % 10;
+    int n3 = n / 10 % 10;
+    int n4 = n % 10;
+
+    if (n1 == n4 && n2 == n3)
+    {
+        Console.WriteLine("Это палиндром!");
+    }
+    else
+        Console.WriteLine("Это не палиндром");
 }
-else
-    Console.WriteLine("Это не палиндром");
+
